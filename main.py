8a96cc2d371f17a6a0a114402da11261446c7654
Bruno Cabral <cabral.brc@gmail.com>
2022-05-18 09:16:43 -0700

    show more than one cube

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Camera:
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        s = dt * 100
+        s = dt * 10
         if keys[pygame.K_q]:
             self.pos[1] += s
         if keys[pygame.K_e]:
@@ -43,6 +43,57 @@ class Camera:
         self.rot[1] += x
 
 
+class Cube:
+    # the cube center is at (0, 0, 0) - (x, y, z)
+    vertices = (
+        (-1, -1, -1),
+        (1, -1, -1),
+        (1, 1, -1),
+        (-1, 1, -1),
+        (-1, -1, 1),
+        (1, -1, 1),
+        (1, 1, 1),
+        (-1, 1, 1),
+    )
+
+    edges = (
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 0),
+        (4, 5),
+        (5, 6),
+        (6, 7),
+        (7, 4),
+        (0, 4),
+        (1, 5),
+        (2, 6),
+        (3, 7),
+    )
+
+    faces = (
+        (0, 1, 2, 3),
+        (4, 5, 6, 7),
+        (0, 1, 5, 4),
+        (2, 3, 7, 6),
+        (0, 3, 7, 4),
+        (1, 2, 6, 5),
+    )
+
+    colors = (
+        (255, 0, 0),
+        (255, 128, 0),
+        (255, 255, 0),
+        (255, 255, 255),
+        (0, 0, 255),
+        (0, 255, 0),
+    )
+
+    def __init__(self, pos=(0, 0, 0)):
+        x, y, z = pos
+        self.verts = [(x + X / 2, y + Y / 2, z + Z / 2) for X, Y, Z in self.vertices]
+
+
 class GameWindow:
     def __init__(self):
         pygame.init()
@@ -56,57 +107,18 @@ class GameWindow:
         self.dt = 0
         self.fps = 60
 
-        # the cube center is at (0, 0, 0) - (x, y, z)
-        self.verts = (
-            (-1, -1, -1),
-            (1, -1, -1),
-            (1, 1, -1),
-            (-1, 1, -1),
-            (-1, -1, 1),
-            (1, -1, 1),
-            (1, 1, 1),
-            (-1, 1, 1),
-        )
-
-        self.edges = (
-            (0, 1),
-            (1, 2),
-            (2, 3),
-            (3, 0),
-            (4, 5),
-            (5, 6),
-            (6, 7),
-            (7, 4),
-            (0, 4),
-            (1, 5),
-            (2, 6),
-            (3, 7),
-        )
-
-        self.faces = (
-            (0, 1, 2, 3),
-            (4, 5, 6, 7),
-            (0, 1, 5, 4),
-            (2, 3, 7, 6),
-            (0, 3, 7, 4),
-            (1, 2, 6, 5),
-        )
-
-        self.colors = (
-            (255, 0, 0),
-            (255, 128, 0),
-            (255, 255, 0),
-            (255, 255, 255),
-            (0, 0, 255),
-            (0, 255, 0),
-        )
-
         self.camera = Camera((0, 0, -5))
 
         # pygame.event.get()
         # pygame.mouse.get_rel()
         pygame.mouse.set_visible(False)
         pygame.event.set_grab(True)
+
+        self.cubes = [
+            Cube((0, 0, 0)),
+            Cube((-2, 0, 0)),
+            Cube((2, 0, 0)),
+        ]
 
     def run(self):
         self.dt = time.time() - self.last_time
@@ -124,57 +136,69 @@ class GameWindow:
                         pygame.quit()
                         sys.exit()
 
-            # update verts coordinates from camera position
-            vert_list = []
-            screen_coords = []
-            for x, y, z in self.verts:
-                x -= self.camera.pos[0]
-                y -= self.camera.pos[1]
-                z -= self.camera.pos[2]
-
-                x, z = self.rotate2d((x, z), self.camera.rot[1])
-                y, z = self.rotate2d((y, z), self.camera.rot[0])
-
-                vert_list.append((x, y, z))
-
-                f = self.center_width / z
-                x, y = x * f, y * f
-                screen_coords.append(
-                    (self.center_width + int(x), self.center_height + int(y))
-                )
-
-            self.draw_edges()
+            # loop through all objects to get all faces
 
             face_list = []
             face_color = []
             depth = []
-            for face_index, face in enumerate(self.faces):
-                x, y = screen_coords[face_index]
-                on_screen = False
-                for face_vertice in face:
-                    z = vert_list[face_vertice][2]
 
-                    # check if face is on screen
-                    if z > 0 and x > 0 and x < self.width and y > 0 and y < self.height:
-                        on_screen = True
-                        break
+            for obj in self.cubes:
 
-                if on_screen:
-                    coords = [screen_coords[on_index] for on_index in face]
-                    face_list.append(coords)
-                    face_color.append(self.colors[face_index])
+                # update verts coordinates from camera position
+                vert_list = []
+                screen_coords = []
+                for x, y, z in obj.verts:
+                    x -= self.camera.pos[0]
+                    y -= self.camera.pos[1]
+                    z -= self.camera.pos[2]
 
-                    depth.append(
-                        sum(
-                            sum(
-                                vert_list[face_screen_index][p_index]
-                                for face_screen_index in face
-                            )
-                            ** 2
-                            for p_index in range(3)
-                        )
+                    x, z = self.rotate2d((x, z), self.camera.rot[1])
+                    y, z = self.rotate2d((y, z), self.camera.rot[0])
+
+                    vert_list.append((x, y, z))
+
+                    f = self.center_width / z
+                    x, y = x * f, y * f
+                    screen_coords.append(
+                        (self.center_width + int(x), self.center_height + int(y))
                     )
 
+                self.draw_edges(obj)
+
+                for face_index, face in enumerate(obj.faces):
+                    x, y = screen_coords[face_index]
+                    on_screen = False
+                    for face_vertice in face:
+                        z = vert_list[face_vertice][2]
+
+                        # check if face is on screen
+                        if (
+                            z > 0
+                            and x > 0
+                            and x < self.width
+                            and y > 0
+                            and y < self.height
+                        ):
+                            on_screen = True
+                            break
+
+                    if on_screen:
+                        coords = [screen_coords[on_index] for on_index in face]
+                        face_list.append(coords)
+                        face_color.append(obj.colors[face_index])
+
+                        depth.append(
+                            sum(
+                                sum(
+                                    vert_list[face_screen_index][p_index]
+                                    for face_screen_index in face
+                                )
+                                ** 2
+                                for p_index in range(3)
+                            )
+                        )
+
+            # draw all faces from all objects
             order = sorted(range(len(face_list)), key=lambda i: depth[i], reverse=True)
             for order_index in order:
                 try:
@@ -189,10 +213,10 @@ class GameWindow:
 
             self.camera.update(self.dt)
 
-    def draw_edges(self):
+    def draw_edges(self, obj):
 
-        for edge in self.edges:
-            vertices = self.verts[edge[0]], self.verts[edge[1]]
+        for edge in obj.edges:
+            vertices = obj.verts[edge[0]], obj.verts[edge[1]]
             points = []
             for x, y, z in vertices:
 
