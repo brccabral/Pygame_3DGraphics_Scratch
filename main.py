4f27565ce306b23195577efa1bd1acab5bf6af45
Bruno Cabral <cabral.brc@gmail.com>
2022-05-17 20:03:25 -0700

    added camera, can move around

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ import pygame
 import sys
 
 
+class Camera:
+    def __init__(self, pos=(0, 0, 0), rot=(0, 0)):
+        # receives tuple, but need to convert to list because
+        # tuples are immutable and we need to change them
+        self.pos = list(pos)
+        self.rot = list(rot)
+
+    def update(self, dt, keys):
+        s = dt * 10000
+        if keys[pygame.K_q]:
+            self.pos[1] += s
+        if keys[pygame.K_e]:
+            self.pos[1] -= s
+
+        if keys[pygame.K_w]:
+            self.pos[2] += s
+        if keys[pygame.K_s]:
+            self.pos[2] -= s
+        if keys[pygame.K_a]:
+            self.pos[0] -= s
+        if keys[pygame.K_d]:
+            self.pos[0] += s
+
+
 class GameWindow:
     def __init__(self):
         pygame.init()
@@ -43,6 +67,8 @@ class GameWindow:
             (3, 7),
         )
 
+        self.camera = Camera((0, 0, -5))
+
     def run(self):
         self.dt = time.time() - self.last_time
         self.last_time = time.time()
@@ -54,27 +80,15 @@ class GameWindow:
                     pygame.quit()
                     sys.exit()
 
-            for x, y, z in self.verts:
-                # shift in z axis
-                z += 5
-
-                # projection coefficient
-                # using because it is a square screen, in tutorial they use 200 hardcoded
-                f = self.center_width / z
-                x, y = x * f, y * f
-
-                pygame.draw.circle(
-                    self.screen,
-                    (255, 255, 255),
-                    (self.center_width + int(x), self.center_height + int(y)),
-                    6,
-                )
-
             for edge in self.edges:
                 vertices = self.verts[edge[0]], self.verts[edge[1]]
                 points = []
                 for x, y, z in vertices:
-                    z += 5
+
+                    x -= self.camera.pos[0]
+                    y -= self.camera.pos[1]
+                    z -= self.camera.pos[2]
+
                     f = self.center_width / z
                     x, y = x * f, y * f
                     points.append(
@@ -84,6 +98,9 @@ class GameWindow:
 
             pygame.display.flip()
             self.clock.tick(self.fps)
+
+            keys = pygame.key.get_pressed()
+            self.camera.update(self.dt, keys)
 
 
 if __name__ == "__main__":
