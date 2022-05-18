c87b4238028a270633d5e9dda02b8f498b6926ec
Bruno Cabral <cabral.brc@gmail.com>
2022-05-17 19:54:26 -0700

    draw cube edges

--- main.py
+++ main.py
@@ -28,6 +28,21 @@ class GameWindow:
             (-1, 1, 1),
         )
 
+        self.edges = (
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 0),
+            (4, 5),
+            (5, 6),
+            (6, 7),
+            (7, 4),
+            (0, 4),
+            (1, 5),
+            (2, 6),
+            (3, 7),
+        )
+
     def run(self):
         self.dt = time.time() - self.last_time
         self.last_time = time.time()
@@ -54,6 +69,18 @@ class GameWindow:
                     (self.center_width + int(x), self.center_height + int(y)),
                     6,
                 )
+
+            for edge in self.edges:
+                vertices = self.verts[edge[0]], self.verts[edge[1]]
+                points = []
+                for x, y, z in vertices:
+                    z += 5
+                    f = self.center_width / z
+                    x, y = x * f, y * f
+                    points.append(
+                        (self.center_width + int(x), self.center_height + int(y))
+                    )
+                pygame.draw.line(self.screen, (255, 255, 255), points[0], points[1], 1)
 
             pygame.display.flip()
             self.clock.tick(self.fps)
