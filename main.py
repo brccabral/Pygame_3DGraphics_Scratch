e2099448daf36e33343f772c3be17dd0e20f3ff5
Bruno Cabral <cabral.brc@gmail.com>
2022-05-17 19:46:19 -0700

    draw cube vertices

--- main.py
+++ main.py
@@ -10,22 +10,50 @@ class GameWindow:
         self.height = 400
         self.center_width = self.width // 2
         self.center_height = self.height // 2
-        self.window_surface = pygame.display.set_mode((self.width, self.height))
+        self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
         self.last_time = time.time()
         self.dt = 0
         self.fps = 60
 
+        # the cube center is at (0, 0, 0) - (x, y, z)
+        self.verts = (
+            (-1, -1, -1),
+            (1, -1, -1),
+            (1, 1, -1),
+            (-1, 1, -1),
+            (-1, -1, 1),
+            (1, -1, 1),
+            (1, 1, 1),
+            (-1, 1, 1),
+        )
+
     def run(self):
         self.dt = time.time() - self.last_time
         self.last_time = time.time()
         while True:
-            self.window_surface.fill("black")
+            self.screen.fill("black")
 
             for event in pygame.event.get([pygame.QUIT]):
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+            for x, y, z in self.verts:
+                # shift in z axis
+                z += 5
+
+                # projection coefficient
+                # using because it is a square screen, in tutorial they use 200 hardcoded
+                f = self.center_width / z
+                x, y = x * f, y * f
+
+                pygame.draw.circle(
+                    self.screen,
+                    (255, 255, 255),
+                    (self.center_width + int(x), self.center_height + int(y)),
+                    6,
+                )
 
             pygame.display.flip()
             self.clock.tick(self.fps)
